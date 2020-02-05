e9e6fb3b26ff7914c7ebca07d274c516f945d13f
DENIS SAVGIR <denis@DENISs-MacBook-Pro.local>
2020-02-05 11:45:15 -0500

    Done.

--- calculate.js
+++ calculate.js
@@ -2,7 +2,38 @@
  * YOUR CODE HERE *
  ******************/
 
+function calculate(num1, num2, operator) {
+  num1 = Number(num1);
+  num2 = Number(num2);
 
+  switch(operator) {
+    case '+':
+    case 'plus':
+    case 'added to':
+      return num1 + num2;
+    case '-':
+    case 'minus':
+    case 'subtracted from':
+      return num1 - num2;
+    case '*':
+    case 'x':
+    case 'X':
+    case 'multiply':
+    case 'times':
+    case 'multiplied by':
+      return num1 * num2;
+    case '/':
+    case 'divide':
+    case 'divided by':
+      return num1 / num2;
+    case '%':
+    case 'mod':
+    case 'modulus':
+      return num1 % num2;
+    default:
+      return "Sorry, that's not a mathematical operation!"
+  }
+}
  
 
 /********************************************
